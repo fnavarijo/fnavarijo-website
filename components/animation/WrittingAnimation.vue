e9f26d9d4be73b9efc8efc8ae74854981fd6df6b
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2020-09-07 19:21:17 -0600

    Update: adding nuxt-property-decorator library

--- components/animation/WrittingAnimation.vue
+++ components/animation/WrittingAnimation.vue
@@ -5,9 +5,7 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import { Prop } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
 
 @Component
 export default class WrittingAnimation extends Vue {
